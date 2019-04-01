bc6597b0dd17f86cd65559c2a0a0212fd245ed85
IinRahmawati <48981585+IinRahmawati@users.noreply.github.com>
2019-04-01 19:00:42 +0700

    Create soal1.cpp

---
+++ soal1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Nama           : Iin Rahmawati"<<endl;
+  cout << "NIM            : 1829140016"<<endl;
+  cout << "kelas          : tekom B"<<endl;
+  cout << "Tahun angkatan :2018"<<endl;
+
+retun 0;
+}
